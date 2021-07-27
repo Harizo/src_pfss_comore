d69fdc4c55b66caf947f3b1d80953b720cc25f28
aime_bruno <ndrianaina.aime.bruno@gmail.com>
2021-07-27 14:44:53 +0300

    Groupe ML/PL : ménage à enlever dans la liste de ML/PL si déjà affécté à un groupe

--- app/index.constants.js
+++ app/index.constants.js
@@ -5,22 +5,22 @@
     angular
         .module("fuse")
 
-		/* .constant("apiUrl", "http://localhost/2020/pfss_comores/api/index.php/api/")
+		 .constant("apiUrl", "http://localhost/2020/pfss_comores/api/index.php/api/")
 		.constant("apiUrlbase", "http://localhost/2020/pfss_comores/")
 		.constant('apiUrlExcel', "http://localhost/2020/pfss_comores/exportexcel/")
 		.constant('apiUrlExcelimport', "http://localhost/2020/pfss_comores/importexcel/")
 		.constant('apiUrlReporting', "http://localhost/2020/pfss_comores/reporting/")
 		.constant("apiUrl_serve_central", "http://localhost/2020/pfss_comores/api/index.php/api/")//ip serveur cantrale si dans les ile pour l'envoi des données
-		.constant("serveur_central", true) */
+		.constant("serveur_central", true) 
 
 		//SERVEUR LOCAL
-			.constant("apiUrl", "https://mis.island-agro.com/2020/pfss_comores/api/index.php/api/")
-			.constant("apiUrlbase", "https://mis.island-agro.com/2020/pfss_comores/")
-			.constant('apiUrlExcel', "https://mis.island-agro.com/2020/pfss_comores/exportexcel/")
-			.constant('apiUrlExcelimport', "https://mis.island-agro.com/2020/pfss_comores/importexcel/")
-			.constant('apiUrlReporting', "https://mis.island-agro.com/2020/pfss_comores/reporting/")
-			.constant("apiUrl_serve_central", "https://mis.island-agro.com/2020/pfss_comores/api/index.php/api/")//ip serveur cantrale si dans les ile pour l'envoi des données
-			.constant("serveur_central", true)
+			// .constant("apiUrl", "https://mis.island-agro.com/2020/pfss_comores/api/index.php/api/")
+			// .constant("apiUrlbase", "https://mis.island-agro.com/2020/pfss_comores/")
+			// .constant('apiUrlExcel', "https://mis.island-agro.com/2020/pfss_comores/exportexcel/")
+			// .constant('apiUrlExcelimport', "https://mis.island-agro.com/2020/pfss_comores/importexcel/")
+			// .constant('apiUrlReporting', "https://mis.island-agro.com/2020/pfss_comores/reporting/")
+			// .constant("apiUrl_serve_central", "https://mis.island-agro.com/2020/pfss_comores/api/index.php/api/")//ip serveur cantrale si dans les ile pour l'envoi des données
+			// .constant("serveur_central", true)
 
 
 		//FIN SERVEUR LOCAL
